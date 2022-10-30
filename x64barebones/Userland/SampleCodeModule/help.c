be5d44a5a76c7347e9dce72ef7a1a2f7dfe0360c
smedin <95591645+smedin@users.noreply.github.com>
2022-10-30 20:29:15 -0300

    real betis balompie

--- x64barebones/Userland/SampleCodeModule/help.c
+++ x64barebones/Userland/SampleCodeModule/help.c
@@ -2,16 +2,16 @@
 #include <functions.h>
 
 const char* helpString = 
-  "clear                  Clears screen's content\n"
-  "decreasefontsize				Reduces the font size.\n"
-	"divbyzero					    Simulates the error that occurs when dividing by zero.\n"
-	"help							      Prints all commands and their description.\n"
-	"increasefontsize				Reduces the font size.\n"
-	"inforeg						    Prints the value of the registers when screenshotted. Use ; to screenshot\n"
-	"invalidopcode		      Simulates the error that occurs when using an invalid operation code.\n"
-  "printmem						    Given an address pointer prints the following 32 bytes.\n"
-	"time							      Prints the current time.\n"
-	"tron         					Executes the game tron light cycle. It requires two players, one will move with the arrow-keys and the other with the keys W A S D.\n";
+  	"clear           							Clears screen's content\n"
+  	"decreasefontsize				   Reduces the font size.\n"
+	"divbyzero								      Simulates the error that occurs when dividing by zero.\n"
+	"help				               Prints all commands and their description.\n"
+	"increasefontsize				   Increases the font size.\n"
+	"inforeg						 				     Prints the value of the registers when screenshotted. Use ; to screenshot\n"
+	"invalidopcode							   Simulates the error that occurs when using an invalid operation code.\n"
+  	"printmem							  		    Given an address pointer prints the following 32 bytes.\n"
+	"time				               Prints the current time.\n"
+	"tron				               Executes the game tron light cycle.\n";
 
 void help() {
 	print(helpString, strlen(helpString));
--- x64barebones/run.sh
+++ x64barebones/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # if you have windows use this
-# sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
+sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
 # if you hava mac use this
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 #-audiodev sdl,id=audio0 -machine pcspk-audiodev=audio0
+# qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 #-audiodev sdl,id=audio0 -machine pcspk-audiodev=audio0
